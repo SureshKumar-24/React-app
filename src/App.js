79694520dd1031875b675cef0b230015cec2a566
zeniddeveloper02 <145428575+zeniddeveloper02@users.noreply.github.com>
2023-10-11 23:37:47 +0530

    props-passing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
 import Student from './Student';
 
 function App() {
+  const [name, setName] = useState("Sam")
   return (
     <div className='App'>
       <h1>Props in React </h1>
-      <Student name={"Sam"} email={"sam123@gmail.com"} other={{ address: "delhi", pincode: 15215 }} />
-      <Student name={"Peter"} email={"peterkim123@gmail.com"} other={{ address: "Mohali", pincode: 152145 }} />
-      <Student name={"John"} email={"john123@gmail.com"} other={{ address: "Chandigarh", pincode: 154788 }} />
+      <Student name={name} />
+      <button onClick={() => { setName("karan") }}>Click Me</button>
     </div>
   )
 }
--- src/Student.js
+++ src/Student.js
@@ -1,10 +1,8 @@
 function Student(props) {
-    console.log(props.other);
+    console.log(props);
     return (
         <div style={{ backgroundColor: "skyblue", margin: 10 }}>
             <h3>Name = {props.name}</h3>
-            <h3>Email = {props.email}</h3>
-            <h4>Address = {props.other.address}   pincode ={props.other.pincode}</h4>
         </div>
     )
 }
