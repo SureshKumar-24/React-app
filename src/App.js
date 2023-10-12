d272cc89b2cff047f06d017b90df2d00351ef16a
zeniddeveloper02 <145428575+zeniddeveloper02@users.noreply.github.com>
2023-10-12 22:46:36 +0530

    input value

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Student from './Student';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "Anil"
-    }
+function App() {
+  const [data, setData] = useState(null);
+  const [print, setPrint] = useState(false);
+  function getdata(val) {
+    console.log(val.target.value);
+    setData(val.target.value);
+    setPrint(false);
   }
-  render() {
-    return (
-      <div className="App">
-        <h1>Props in React </h1>
-        <Student name={this.state.name} email="anil123@gmail.com" />
-        <button onClick={() => this.setState({ name: "Sidhu" })}>click Me</button>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {
+        print ?
+          <h1>{data}</h1>
+          : null
+      }
+      <input type="text" onChange={getdata}></input>
+      <button onClick={() => setPrint(true)}>Print Value</button>
+    </div>
+  )
 }
 
 
